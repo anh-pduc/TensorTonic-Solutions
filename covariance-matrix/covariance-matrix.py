d0640d7cc69ef7628be83bfff80b02c92d122137
anh-pduc <ducducanh2204@gmail.com>
2026-03-28 00:54:01 +0700

    Update solution for covariance-matrix

--- covariance-matrix/covariance-matrix.py
+++ covariance-matrix/covariance-matrix.py
@@ -12,6 +12,6 @@ def covariance_matrix(X):
     X_cen_t = np.transpose(X_cen)
     n = X.shape[0]
     cov = np.dot(X_cen_t,X_cen)
-    cov = 1/(n-1+1e-8)*cov
+    cov = 1/(n-1)*cov
     return cov
     pass

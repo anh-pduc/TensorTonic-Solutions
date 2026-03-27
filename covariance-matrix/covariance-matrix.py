a506c9b35b4f5ae506b6862dd36b612008f8c6d2
anh-pduc <ducducanh2204@gmail.com>
2026-03-28 00:53:35 +0700

    Add solution for covariance-matrix

---
+++ covariance-matrix/covariance-matrix.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def covariance_matrix(X):
+    """
+    Compute covariance matrix from dataset X.
+    """
+    X = np.asarray(X,dtype=float)
+    if X.ndim != 2 or X.shape[0] <2:
+        return None
+    mu = np.mean(X, axis = 0)
+    X_cen = X - mu
+    X_cen_t = np.transpose(X_cen)
+    n = X.shape[0]
+    cov = np.dot(X_cen_t,X_cen)
+    cov = 1/(n-1+1e-8)*cov
+    return cov
+    pass
